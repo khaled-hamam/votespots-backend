df5b5864ecc4ed322259f7ebfcafde7abdf991b2
Khaled Mohamed <khal3d.mohamed@gmail.com>
2018-08-24 06:32:23 +0200

    Initial commit

---
+++ src/controllers/index.ts
@@ -0,0 +1,6 @@
+import { IController } from './IController';
+import HomeController from './HomeController';
+
+const controllers: IController[] = [new HomeController()];
+
+export default controllers;
