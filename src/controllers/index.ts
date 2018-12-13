3dfdb306a7e7152b02ecc64b28f929b57f104063
mariamkamel <mariammkamel98@gmail.com>
2018-12-13 14:48:00 +0200

    vote controller/schema added

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,6 +1,7 @@
 import { IController } from './IController';
 import HomeController from './HomeController';
+import VoteController from './VoteController';
 
-const controllers: IController[] = [new HomeController()];
+const controllers: IController[] = [new HomeController(), new VoteController()];
 
 export default controllers;
