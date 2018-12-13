e2a5772710fca334744b048bbafbfb70f5d700c0
mariamkamel <mariammkamel98@gmail.com>
2018-12-13 17:02:08 +0200

    vote validations added

--- src/utils/validation/vote.ts
+++ src/utils/validation/vote.ts
@@ -1,10 +1,15 @@
 export function validateVoteInput(body) {
   let isValid = true;
-  const { name } = body;
+  const { name, headers } = body;
   let msg = '';
 
   if (name === ' ' || !name) {
     msg = 'Please enter name';
+    isValid = false;
+  }
+
+  if (!headers || headers.length === 0) {
+    msg = 'please enter at least one header';
     isValid = false;
   }
   return { isValid, msg };
